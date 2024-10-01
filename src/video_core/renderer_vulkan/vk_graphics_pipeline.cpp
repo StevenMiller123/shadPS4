b605e647cef69204adfd2e870922154270e340bb
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2024-10-01 09:57:03 -0500

    hack 2

--- src/video_core/renderer_vulkan/vk_graphics_pipeline.cpp
+++ src/video_core/renderer_vulkan/vk_graphics_pipeline.cpp
@@ -434,7 +434,7 @@ void GraphicsPipeline::BindResources(const Liverpool::Regs& regs,
                            // "Texel buffer stride must match format stride");
                 const u32 offset_aligned = Common::AlignDown(offset, alignment);
                 const u32 adjust = offset - offset_aligned;
-                ASSERT(adjust % fmt_stride == 0);
+                // ASSERT(adjust % fmt_stride == 0);
                 push_data.AddOffset(binding.buffer, adjust / fmt_stride);
                 buffer_view = vk_buffer->View(offset_aligned, size + adjust, desc.is_written,
                                               vsharp.GetDataFmt(), vsharp.GetNumberFmt());
